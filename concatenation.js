2a7bbd9124064167c49626c4ceacfe14da3279f4
ahbanna <alhasanulbanna@gmail.com>
2023-12-23 00:25:15 +0600

    concatenation, data types

---
+++ concatenation.js
@@ -0,0 +1,12 @@
+console.log("Hasanul " + "Banna"); // Hasanul Banna
+console.log(5 + 10); // 15
+console.log("5" + 5); // 55
+console.log("My roll numebr is:" + 50); // My roll number is: 50.
+
+let name = "Banna";
+console.log("My name is" + name); // My name is Banna. Here name is a variable
+
+console.log(`I am ${10 + 20} years old`); // I am 30 years old
+
+console.log([1, 2, 3]); // Printing arrays
+console.log({ name: "John", age: 30 }); // Printing objects
---
+++ data-types.js
@@ -0,0 +1,92 @@
+// --------------- Data types --------------------
+// String
+let greeting = "Hello, World!";
+console.log(typeof greeting); //  "string"
+
+// Number
+let num = 42;
+console.log(typeof num); //"number"
+
+// Boolean
+let isMarried = true;
+console.log(typeof isMarried); // boolean
+
+// Null
+let a = null;
+console.log(a); // null
+
+// Undefined
+let fullName;
+console.log(typeof fullName);
+
+// Object
+let userInfo = {
+  name: "Banna",
+  age: 20,
+};
+console.log(userInfo.name); // Banna
+console.log(typeof userInfo); // object
+
+// Array
+let friends = ["Hasan", "kamal", "jamal"];
+console.log(friends[1]); // Kamal
+console.log(typeof friends); // Object
+
+// Function
+function user(name) {
+  console.log(`My name is ${name}`);
+}
+user("Banna"); //  My name is Banna
+console.log(typeof user); // function
+
+// Immutable
+// Example: 1
+let name1 = "Banna";
+let name2 = name1; // name2 now has the same value as name1, which is "Banna"
+name2 = "Tutul"; // name2 is assigned a new value "Tutul"
+console.log(name1); // "Banna" - Because name1 was never changed
+console.log(name2); // "Tutul" - name2 now holds the value "Tutul"
+
+// Example: 2
+const teacher = "Tiger sir";
+teacher[0] = "Tofazzal";
+console.log(teacher); // output: Tiger sir
+
+// Mutable
+// Example: 1
+let person1 = {
+  name: "Ruble",
+  age: 25,
+};
+let person2 = person1; //  Both variables point to the same object in memory.
+person2.name = "Manna"; // This modifies the name property of the shared object. Both person and person2 reference this object, so the change affects both variables.
+console.log(person1.name); // Manna
+console.log(person2.name); // Manna
+
+// Example: 1.1
+let user1 = {
+  name: "Masum",
+  age: 25,
+};
+let user2 = { ...user1 };
+user2.name = "Anowar";
+console.log(user1.name); // Masum
+console.log(user2.name); // Anowar
+
+// Example: 2
+const obj1 = {
+  a: 1,
+  b: 2,
+};
+const obj2 = obj1;
+obj2.a = 5;
+console.log(obj1.a); // 5
+console.log(obj2.a); // 5
+
+// Example: 3
+let peopleList = ["Arnold", "Linda", "Sylvester", "Dolph"];
+peopleList[0] = "Jean-Claude";
+
+console.log(peopleList); // [ 'Jean-Claude', 'Linda', 'Sylvester', 'Dolph' ]
+
+
