3db4e967fced75b76a7b097ee5d2cf98b49edb29
ahbanna <alhasanulbanna@gmail.com>
2024-01-01 15:00:05 +0600

    array & object destructuring

---
+++ array-destructuring.js
@@ -0,0 +1,40 @@
+// Example: 1
+/*const myFevoriteFood = ["rice", "mango", "apple", "zambura"];
+console.log(food[2]);   //apple
+//Or, 
+const [one, two, three, four] = myFevoriteFood;
+console.log(three);		 // apple
+*/
+//Example: 1.1
+/*
+const myFevoriteFood = ["rice", "mango", "apple", "zambura"];
+const shakilaFevoriteFood = [...myFevoriteFood];
+shakilaFevoriteFood.push("Hilsha Fish");
+console.log(shakilaFevoriteFood); //[ 'rice', 'mango', 'apple', 'zambura', 'Hilsha Fish' ]
+console.log(myFevoriteFood); //  [ 'rice', 'mango', 'apple', 'zambura' ]
+*/
+// Example: 2
+/*
+const users = [
+  {
+    name: "Shakila",
+    age: 30,
+    address: {
+      city: "New York",
+      state: "NY",
+    },
+  },
+  {
+    name: "Banna",
+    age: 25,
+    address: {
+      city: "Los Angeles",
+      state: "CA",
+    },
+  },
+];
+
+const [firstUser, secondUser] = users;
+console.log(firstUser.name); // Shakila
+console.log(firstUser.address.city); // New York
+*/
--- object-destructuring.js
+++ object-destructuring.js
@@ -1,5 +1,5 @@
-// Example: 
-const tutul = {
+// Example: 1
+/*const tutul = {
   "father's name": "Abu Bakkar Siddique",
   "mother's name": "Anwara Begum",
   address: "Bogra",
@@ -8,3 +8,29 @@ const shohag = { ...tutul };
 shohag.children = "Rufaida";
 console.log(tutul);
 console.log(shohag);
+*/
+// Example: 2
+/*
+const std = {
+  stdName: "Banna",
+  group: "Science",
+  result: {
+    ssc: "A+",
+    hsc: "A",
+  },
+};
+
+// Process: 1
+//  console.log(`Student Name: ${std.stdName} Group: ${std.group} SSC Result: ${std.result.ssc} HSC Result: ${std.result.hsc}
+// `);
+// Process: 2
+// const { stdName, group } = std;
+// const { ssc, hsc } = std.result;
+// console.log(
+//   `Student Name: ${stdName} Group: ${group} SSC Result: ${ssc} HSC Result: ${hsc}`
+// );
+// Process: 3 (Mostly used)
+const { stdName, group, result } = std;
+console.log(stdName);
+console.log(result.ssc);
+*/
